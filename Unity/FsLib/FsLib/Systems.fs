5175263bed857bd9e9a783a28dc01c10210e0dff
Masahiro Yoshida <myoshida@aiming-inc.com>
2024-04-14 14:19:04 +0900

    add system, world

---
+++ Unity/FsLib/FsLib/Systems.fs
@@ -0,0 +1,17 @@
+﻿module FsLib.Systems
+
+open System.Numerics
+open FsLib.Components
+
+
+let calcVelocityWithInput _ input =
+    let vx = if input.Left then -2.0f elif input.Right then 2.0f else 0.0f
+    let vy = if input.Up then -2.0f elif input.Down then 2.0f else 0.0f
+
+    Vector2(vx, vy)
+    |> Velocity
+    
+let calcPosition (Position current) (Velocity velocity) =
+    current + velocity
+    |> Position
+    
---
+++ Unity/FsLib/FsLib/World.fs
@@ -0,0 +1,19 @@
+﻿module FsLib.World
+
+open FsLib.EntityComponent
+open FsLib.Components
+
+[<Struct>]
+type World = {
+    Inputs: Component<Input> list
+    Velocities: Component<Velocity> list
+    CurrentPositions: Component<Position> list }
+
+let NewWorld = {
+    Inputs = List.Empty
+    Velocities = List.Empty
+    CurrentPositions = List.Empty }
+
+// let AddHero (world:World) (Position position) (Health health) =
+//     world.Velocities = List.Empty
+    

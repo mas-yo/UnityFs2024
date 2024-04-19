6dd1f39e6f3d4b9e0e4b6d672f202b8dbfc0a34f
Masahiro Yoshida <myoshida@aiming-inc.com>
2024-04-20 00:03:02 +0900

    fix move direction

--- Unity/FsLib/FsLib/Systems.fs
+++ Unity/FsLib/FsLib/Systems.fs
@@ -18,14 +18,14 @@ let calcAttackAnimation attackAnimation input =
     
 let calcDirectionWithInput (Direction direction) input =
     match input.Input.Left(), input.Input.Right(), input.Input.Up(), input.Input.Down() with
-        | true, false, false, false -> -90f + 360f //MathF.PI
-        | false, true, false, false -> 90f //0f
-        | false, false, true, false -> 0f //MathF.PI * 1.5f
-        | false, false, false, true -> 180f //MathF.PI * 0.5f
-        | true, false, true, false -> -45f + 360f //MathF.PI * 5.0f / 4.0f
-        | true, false, false, true -> -135f + 360f //MathF.PI * 3.0f / 4.0f
-        | false, true, true, false -> 45f //MathF.PI * 7.0f / 4.0f
-        | false, true, false, true -> 135f //MathF.PI / 04.0f
+        | true, false, false, false -> -90f
+        | false, true, false, false -> 90f
+        | false, false, true, false -> 0f
+        | false, false, false, true -> 180f
+        | true, false, true, false -> -45f
+        | true, false, false, true -> -135f
+        | false, true, true, false -> 45f
+        | false, true, false, true -> 135f
         | _ -> direction
     
     |> Direction
@@ -34,7 +34,7 @@ let calcDirectionWithInput (Direction direction) input =
 
 let calcVelocityWithInput _ input =
     let vx = if input.Input.Left() then -0.1f elif input.Input.Right() then 0.1f else 0.0f
-    let vy = if input.Input.Up() then -0.1f elif input.Input.Down() then 0.1f else 0.0f
+    let vy = if input.Input.Up() then 0.1f elif input.Input.Down() then -0.1f else 0.0f
 
     Vector2(vx, vy)
     |> Velocity
